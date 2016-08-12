67a8d8911897ed44e6a25176062d6f42bd59e93b
Anton Shemerey <shemerey@gmail.com>
2016-08-12 14:02:43 +0300

    clean up

--- app/reducers/icons.js
+++ app/reducers/icons.js
@@ -3,7 +3,7 @@ import * as icons from 'rebel-icons'
 import _ from 'lodash'
 
 const iconsObj = _.reduce(Object.keys(icons), (acc, key) => {
-  acc[key] = key
+  acc[key] = { key , selected: ('RubyIcon' == key) }
   return acc
 }, {})
 
@@ -22,45 +22,4 @@ export default handleActions({
     }
   },
 
-  // 'add todo' (state, action) {
-  //   return [{
-  //     id: state.reduce((maxId, todo) => Math.max(todo.id, maxId), -1) + 1,
-  //     completed: false,
-  //     text: action.payload
-  //   }, ...state]
-  // },
-  //
-  // 'delete todo' (state, action) {
-  //   return state.filter(todo => todo.id !== action.payload )
-  // },
-  //
-  // 'edit todo' (state, action) {
-  //   return state.map(todo => {
-  //     return todo.id === action.payload.id
-  //       ? { ...todo, text: action.payload.text }
-  //       : todo
-  //   })
-  // },
-  //
-  // 'complete todo' (state, action) {
-  //   return state.map(todo => {
-  //     return todo.id === action.payload
-  //       ? { ...todo, completed: !todo.completed }
-  //       : todo
-  //   })
-  // },
-  //
-  // 'complete all' (state, action) {
-  //   const areAllMarked = state.every(todo => todo.completed)
-  //   return state.map(todo => {
-  //     return {
-  //       ...todo,
-  //       completed: !areAllMarked
-  //     }
-  //   })
-  // },
-  //
-  // 'clear complete' (state, action) {
-  //   return state.filter(todo => todo.completed === false)
-  // }
 }, initialState)
